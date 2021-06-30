31b938e97e12d5d0d868679140ba9e7b4d17c8ab
Беляев Михаил <belyaevmsmail@yandex.ru>
2021-06-30 23:31:56 +0300

    Initial commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using IronBarCode;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tryImage = new Bitmap("C:\\MeterPhotos\\456.png");
+            var dataByteArray = BarcodeReader.QuicklyReadOneBarcode(tryImage);
+            if (dataByteArray == null)
+            {
+                Console.WriteLine("null");
+            }
+            else
+            {
+                Console.WriteLine(dataByteArray.Text);
+            }
+        }
+    }
+}
